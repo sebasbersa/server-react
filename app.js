ea166029c64b51475a47a66411cf4c6acd227173
Sebastian Berrios <sebas.ing.civ.berrios@gmail.com>
2021-07-22 10:36:25 -0400

    inicial

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const {API_VERSION} = require("./config");
+
+//load routing
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//configure header HTTP
+
+//Router Basic
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,9 @@
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+const portDb = "27017";
+
+module.exports = {
+    API_VERSION,
+    IP_SERVER,
+    portDb    
+};
